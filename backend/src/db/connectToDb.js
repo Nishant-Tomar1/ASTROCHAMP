fa1237a1c77f63afd7e0649c3e0be4de84367a02
Nishant <nishanttomar910@gmail.com>
2024-01-08 15:07:15 +0530

    done on render

--- backend/src/db/connectToDb.js
+++ backend/src/db/connectToDb.js
@@ -1,9 +1,8 @@
 import { connect } from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 async function connectToDb(){
     try{       
-        const connectionInstance = await connect(`${process.env.DB_URL}/${DB_NAME}`)
+        const connectionInstance = await connect(`${process.env.DB_URL}`)
         console.log(`MongoDB database connected!! Db URL : ${connectionInstance.connection.host}`)
 
     }catch(err) {
